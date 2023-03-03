e2fdbef5f6f0aed37a5fd34c87d68ebd3b76f60e
mbasicpalkovic <mario.basicpalkovic@htecgroup.com>
2023-03-03 11:46:30 +0100

    Add implementation of event publishing using custom publishers

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderCreateCommandHandler.java
@@ -2,74 +2,29 @@ package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
 import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
-import com.food.ordering.system.order.service.domain.entity.Order;
-import com.food.ordering.system.order.service.domain.entity.Restaurant;
-import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
 import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
-import com.food.ordering.system.order.service.domain.ports.output.repository.CustomerRepository;
-import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
-import com.food.ordering.system.order.service.domain.ports.output.repository.RestaurantRepository;
-import java.util.Optional;
-import java.util.UUID;
+import com.food.ordering.system.order.service.domain.ports.output.message.publishers.payment.OrderCreatedPaymentRequestMessagePublisher;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
 @AllArgsConstructor
 public class OrderCreateCommandHandler {
 
-  private final OrderDomainService orderDomainService;
-  private final OrderRepository orderRepository;
-  private final CustomerRepository customerRepository;
-  private final RestaurantRepository restaurantRepository;
   private final OrderDataMapper orderDataMapper;
+  private final OrderCreateHelper orderCreateHelper;
+  private final OrderCreatedPaymentRequestMessagePublisher publisher;
 
 
-  @Transactional
   public CreateOrderResponse createOrder(CreateOrderCommand command) {
-    checkCustomer(command.getCustomerId());
-    var restaurant = checkRestaurant(command);
-    var order = orderDataMapper.toOrderFrom(command);
-    var orderCreatedEvent = orderDomainService.validateAndInitiateOrder(order, restaurant);
+    var orderCreatedEvent = orderCreateHelper.persistOrder(command);
+    log.info("Order is created with id '{}'", orderCreatedEvent.getOrder().getId().getValue());
 
-    var orderResult = saveOrder(order);
-    log.info("Order is created with id '{}'", orderResult.getId().getValue());
+    publisher.publish(orderCreatedEvent);
 
-    return orderDataMapper.toCreatedOrderResponseFrom(orderResult);
-  }
-
-  private Order saveOrder(Order order) {
-    return Optional.ofNullable(orderRepository.save(order))
-        .orElseThrow(() -> {
-          log.error("Could not save order!");
-          throw new OrderDomainException("Could not save order!");
-        });
-  }
-
-  private Restaurant checkRestaurant(CreateOrderCommand createOrderCommand) {
-    Restaurant restaurant = orderDataMapper.toRestaurantFrom(createOrderCommand);
-
-    return restaurantRepository.findRestaurantInformation(restaurant)
-        .orElseThrow(() -> {
-          UUID restaurantId = createOrderCommand.getRestaurantId();
-          log.warn("Could not find restaurant with restaurant id '{}'", restaurantId);
-
-          throw new OrderDomainException("Couldn't find restaurant with id: " + restaurantId);
-        });
-  }
-
-  /**
-   * If we needed to check more things we should do it inside the Customer domain, but since we are
-   * only checking if it exists it's fine.
-   */
-  private void checkCustomer(UUID customerId) {
-    if (customerRepository.findCustomer(customerId).isEmpty()) {
-      log.warn("Could not find customer with customer id: '{}'", customerId);
-      throw new OrderDomainException("Could not find customer with with id: " + customerId);
-    }
+    return orderDataMapper.toCreatedOrderResponseFrom(orderCreatedEvent.getOrder());
   }
 
 }
