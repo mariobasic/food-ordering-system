abfd45d404f9c275afcf1e7851a2e18a1583e19b
mbasicpalkovic <mario.basicpalkovic@htecgroup.com>
2023-04-03 20:32:22 +0200

    Rename PaymentServiceConfig to PaymentServiceConfigData

--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/config/PaymentServiceConfigData.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/config/PaymentServiceConfigData.java
@@ -3,8 +3,8 @@ package com.food.ordering.system.payment.service.domain.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "payment-service")
-public record PaymentServiceConfig(String paymentRequestTopicName,
-                                   String paymentResponseTopicName) {
+public record PaymentServiceConfigData(String paymentRequestTopicName,
+                                       String paymentResponseTopicName) {
 
 
 }
